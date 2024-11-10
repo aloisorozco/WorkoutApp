146dbcd84da8ca3c474acc608477258bc377d68a
aloisorozco <57361077+aloisorozco@users.noreply.github.com>
2024-11-09 21:00:41 -0500

    added ng module for imports and mui

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule,
+    RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
